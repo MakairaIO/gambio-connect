d95436e86f6dfe556c21c8c23da5a330119a10e0
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-01-24 09:55:37 +0100

    MKGO-28
    WIP
    
    Use Database Triggers and stored Procedures

--- GambioConnectInstaller.php
+++ GambioConnectInstaller.php
@@ -9,27 +9,39 @@ class GambioConnectInstaller
 {
     public function __construct(
         protected Connection $connection
-    ) {}
+    ) {
+    }
     
     
-    public function onInstallation() {
-        $this->connection->executeStatement(
-            "CREATE TABLE IF NOT EXISTS `" . ChangesService::TABLE_NAME .  "` (
+    public function onInstallation()
+    {
+        $this->connection->executeStatement("CREATE TABLE IF NOT EXISTS `" . ChangesService::TABLE_NAME . "` (
                 `id` int(11) NOT NULL AUTO_INCREMENT,
                 `gambio_id` varchar(255) NOT NULL,
                 `type` varchar(255) NOT NULL,
-                `comment` varchar(255) NOT NULL,
+                `comment` varchar(255) NOT NULL DEFAULT '',
                 `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 `consumed_at` datetime DEFAULT NULL,
-                PRIMARY KEY (`id`),
-                UNIQUE INDEX idx_unique_gambio_id_type (gambio_id, type)
-              )"
-        );
+                PRIMARY KEY (`id`)
+              )");
+        
+        $this->connection->executeStatement("
+        CREATE PROCEDURE makairaChange (IN id INT, IN entity_type VARCHAR(255))
+            INSERT INTO `makaira_connect_changes` (gambio_id, `type`) VALUES (products_id, entity_type);
+            ");
+        
+        $this->connection->executeStatement("CREATE TRIGGER product_create_trigger AFTER INSERT ON products
+                FOR EACH ROW
+                CALL makairaChange(NEW.products_id, 'product');");
     }
     
-    public function onUninstallation() {
-        $this->connection->executeStatement(
-            "DROP TABLE IF EXISTS " . ChangesService::TABLE_NAME
-        );
+    
+    public function onUninstallation()
+    {
+        $this->connection->executeStatement("DROP TABLE IF EXISTS " . ChangesService::TABLE_NAME);
+        
+        $this->connection->executeStatement("DROP PROCEDURE IF EXISTS makairaProduct;");
+        
+        $this->connection->executeStatement("DROP TRIGGER IF EXISTS product_create_trigger;");
     }
 }
