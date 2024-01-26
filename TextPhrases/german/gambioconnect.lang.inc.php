f38a4b91879b7c18af2d6323491954dc03dbb27a
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-01-26 07:24:12 +0100

    MKGO-29
    Added Translation for CRON Job Configuration

--- TextPhrases/german/gambioconnect.lang.inc.php
+++ TextPhrases/german/gambioconnect.lang.inc.php
@@ -3,9 +3,6 @@
 $t_language_text_section_content_array = [
     'module_description' => 'Mit diesem Module können sie Makaira nutzen',
     'module_title'       => 'Makaira Gambio Connect',
-    'cronjob_title'      => 'Makaira Export Job',
-    'cronjob_label_active' => 'Aktiv',
-    'cronjob_label_interval' => 'Interval',
     'settings'           => 'Einstellungen',
     'makaira_url'        => 'Makaira URL',
     'makaira_instance'   => 'Makaira Instanz',
