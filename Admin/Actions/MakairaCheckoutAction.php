a6fd838f077311e5f697fc203dd45fd912327052
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-01-31 08:29:39 +0100

    MKGO-32
    Removed old Checkout Controller
    
    Use new Checkout Action
    
    Fixed some issues while preparing Stripe Checkout

--- Admin/Actions/MakairaCheckoutAction.php
+++ Admin/Actions/MakairaCheckoutAction.php
@@ -2,16 +2,24 @@
 
 namespace GXModules\Makaira\GambioConnect\Admin\Actions;
 
+use Gambio\Core\Application\Application;
 use Gambio\Core\Application\Http\AbstractAction;
 use Gambio\Core\Application\Http\Request;
 use Gambio\Core\Application\Http\Response;
 use Gambio\Core\Configuration\Services\ConfigurationService;
 use GXModules\Makaira\GambioConnect\Admin\Services\StripeService;
-use LegacyDependencyContainer;
 
 class MakairaCheckoutAction extends AbstractAction
 {
-
+    protected ConfigurationService $configurationService;
+    
+    public function __construct(
+        protected Application $application,
+    ) {
+        $this->configurationService = $this->application->get(ConfigurationService::class);
+    }
+    
+    
     /**
      * @inheritDoc
      */
@@ -19,12 +27,22 @@ class MakairaCheckoutAction extends AbstractAction
     {
         $stripeService = new StripeService();
         
-        $configurationService = LegacyDependencyContainer::getInstance()->get(ConfigurationService::class);
+        $stripeService->setConfigurationService($this->configurationService);
         
-        $stripeService->setConfigurationService($configurationService);
-        
-        foreach($request->getParsedBodyParam('priceIds') as $priceId) {
-            $stripeService->addPriceId($priceId);
+        if($request->getParsedBodyParam(StripeService::BUNDLE_PRICE_ID) === "on") {
+            $stripeService->addPriceId(StripeService::BUNDLE_PRICE_ID);
+        } else {
+            if ($request->getParsedBodyParam(StripeService::SEARCH_PRICE_ID) === "on") {
+                $stripeService->addPriceId(StripeService::SEARCH_PRICE_ID);
+            }
+            
+            if ($request->getParsedBodyParam(StripeService::ADS_PRICE_ID) === "on") {
+                $stripeService->addPriceId(StripeService::ADS_PRICE_ID);
+            }
+            
+            if ($request->getParsedBodyParam(StripeService::RECOMMENDATIONS_PRICE_ID) === "on") {
+                $stripeService->addPriceId(StripeService::RECOMMENDATIONS_PRICE_ID);
+            }
         }
         
         $stripeService->setSuccessUrl($request->getUri()->getHost() . '/admin/makaira/gambio-connect/stripe-checkout-success-callback');
