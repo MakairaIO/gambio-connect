27b5441be2bbcbdc8247d620318b2a82fc86b267
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-01-31 08:32:17 +0100

    MKGO-32
    Simplified price Id addition

--- Admin/Actions/MakairaCheckoutAction.php
+++ Admin/Actions/MakairaCheckoutAction.php
@@ -32,12 +32,10 @@ class MakairaCheckoutAction extends AbstractAction
         if($request->getParsedBodyParam(StripeService::BUNDLE_PRICE_ID) === "on") {
             $stripeService->addPriceId(StripeService::BUNDLE_PRICE_ID);
         } else {
-            if ($request->getParsedBodyParam(StripeService::SEARCH_PRICE_ID) === "on") {
-                $stripeService->addPriceId(StripeService::SEARCH_PRICE_ID);
-            }
-            
-            if ($request->getParsedBodyParam(StripeService::ADS_PRICE_ID) === "on") {
+            if ($request->getParsedBodyParam(StripeService::ADS_PRICE_ID) === "on"
+            || $request->getParsedBodyParam(StripeService::SEARCH_PRICE_ID) === "on") {
                 $stripeService->addPriceId(StripeService::ADS_PRICE_ID);
+                $stripeService->addPriceId(StripeService::SEARCH_PRICE_ID);
             }
             
             if ($request->getParsedBodyParam(StripeService::RECOMMENDATIONS_PRICE_ID) === "on") {
