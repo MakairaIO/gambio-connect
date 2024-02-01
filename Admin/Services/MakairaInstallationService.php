18d33258eecea9e8f6b6b53846467897eeb98427
Hendrik Mennen <77828605+hmennen90@users.noreply.github.com>
2024-02-01 11:43:16 +0100

    Mkgo 34
    Co-authored-by: Dat Lieu <dl@marmalade.group>
    Co-authored-by: Ben Sauer <bs@marmalade.group>

--- Admin/Services/MakairaInstallationService.php
+++ Admin/Services/MakairaInstallationService.php
@@ -6,9 +6,8 @@ use GuzzleHttp\Client;
 use Psr\Http\Message\ResponseInterface;
 
 class MakairaInstallationService
-{
-    
-    //private const URL = 'https://registration.makaira.io/';
+{    
+    //private const URL = 'http://register.makaira.vm/api/register';
     
     private const URL = 'http://register.makaira.vm/';
     
@@ -17,7 +16,7 @@ class MakairaInstallationService
     private const PASSWORD = '6hmtyEQEhecyN2oEDBrZ';
     
     private string $source = 'gambio';
-
+  
     public function __construct(
         protected ?Client $client = null,
         private string    $email = '',
@@ -76,6 +75,40 @@ class MakairaInstallationService
                                        ],
                                    ]);
     }
+    
+    
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+    
+    
+    public function setSubdomain(string $subdomain): void
+    {
+        $this->subdomain = $subdomain;
+    }
+    
+    
+    public function setShopUrl(string $shopUrl): void
+    {
+        $this->shopUrl = $shopUrl;
+    }
+    
+    
+    public function setCheckoutSessionId(string $checkoutSessionId): void
+    {
+        $this->checkoutSessionId = $checkoutSessionId;
+    }
+    
+    public function callRegistrationService(): ResponseInterface
+    {
+        return $this->client->post('', [
+            'source' => $this->source,
+            'subdomain' => $this->subdomain,
+            'email' => $this->email,
+            'shop_url' => $this->shopUrl,
+            'checkout_id' => $this->checkoutSessionId
+    ) 
     
     
     public function setEmail(string $email): static
