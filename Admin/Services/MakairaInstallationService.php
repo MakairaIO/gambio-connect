d1712fe25e7fcf2e603bff0c98261185f483d075
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-02-02 09:07:18 +0100

    MKGO-17
    Fixed MakairaInstallationService.php

--- Admin/Services/MakairaInstallationService.php
+++ Admin/Services/MakairaInstallationService.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client;
 use Psr\Http\Message\ResponseInterface;
 
 class MakairaInstallationService
-{    
+{
     //private const URL = 'http://register.makaira.vm/api/register';
     
     private const URL = 'http://register.makaira.vm/';
@@ -16,79 +16,24 @@ class MakairaInstallationService
     private const PASSWORD = '6hmtyEQEhecyN2oEDBrZ';
     
     private string $source = 'gambio';
-  
+    
+    
     public function __construct(
         protected ?Client $client = null,
         private string    $email = '',
         private string    $subdomain = '',
         private string    $shopUrl = '',
         private string    $checkoutSessionId = ''
-) {
-        $this->client = new Client([
-                                                   'base_uri' => self::URL,
-                                                   'headers'  => [
-                                                       'Authorization' => 'BASIC ' . self::USERNAME . ' '
-                                                                          . self::PASSWORD,
-                                                   ],
-                                               ]);
-    }
-    
-    
-    public function setEmail(string $email): void
-    {
-        $this->email = $email;
-    }
-    
-    
-    public function setSubdomain(string $subdomain): void
-    {
-        $this->subdomain = $subdomain;
-    }
-    
-    
-    public function setShopUrl(string $shopUrl): void
-    {
-        $this->shopUrl = $shopUrl;
-    }
-    
-    
-    public function setCheckoutSessionId(string $checkoutSessionId): void
-    {
-        $this->checkoutSessionId = $checkoutSessionId;
-    }
-    
-    public function callRegistrationService(): ResponseInterface
-    {
-        return $this->client->post('', [
-            'source' => $this->source,
-            'subdomain' => $this->subdomain,
-            'email' => $this->email,
-            'shop_url' => $this->shopUrl,
-            'checkout_id' => $this->checkoutSessionId
     ) {
         $this->client = new Client([
                                        'base_uri' => self::URL,
                                        'headers'  => [
-                                           'Authorization' => 'Basic ' . self::USERNAME . ' ' . self::PASSWORD,
-                                           'Content-Type' => 'application/json',
-                                           'Accept' => 'application/json'
+                                           'Authorization' => 'BASIC ' . self::USERNAME . ' ' . self::PASSWORD,
                                        ],
                                    ]);
     }
     
     
-    public function setEmail(string $email): void
-    {
-        $this->email = $email;
-    }
-    
-    
-    public function setSubdomain(string $subdomain): void
-    {
-        $this->subdomain = $subdomain;
-    }
-    
-    
     public function setShopUrl(string $shopUrl): void
     {
         $this->shopUrl = $shopUrl;
@@ -100,46 +45,16 @@ class MakairaInstallationService
         $this->checkoutSessionId = $checkoutSessionId;
     }
     
-    public function callRegistrationService(): ResponseInterface
-    {
-        return $this->client->post('', [
-            'source' => $this->source,
-            'subdomain' => $this->subdomain,
-            'email' => $this->email,
-            'shop_url' => $this->shopUrl,
-            'checkout_id' => $this->checkoutSessionId
-    ) 
     
-    
-    public function setEmail(string $email): static
+    public function setEmail(string $email): void
     {
         $this->email = $email;
-        
-        return $this;
     }
     
     
-    public function setSubdomain(string $subdomain): static
+    public function setSubdomain(string $subdomain): void
     {
         $this->subdomain = $subdomain;
-        
-        return $this;
-    }
-    
-    
-    public function setShopUrl(string $shopUrl): static
-    {
-        $this->shopUrl = $shopUrl;
-        
-        return $this;
-    }
-    
-    
-    public function setCheckoutSessionId(string $checkoutSessionId): static
-    {
-        $this->checkoutSessionId = $checkoutSessionId;
-        
-        return $this;
     }
     
     public function setCallbackUri(string $callbackUri): static
@@ -154,13 +69,13 @@ class MakairaInstallationService
     {
         return $this->client->post('/api/register', [
             'body' => json_encode([
-                'source'      => $this->source,
-                'subdomain'   => $this->subdomain,
-                'email'       => $this->email,
-                'shop_url'    => $this->shopUrl,
-                'stripe_checkout_id' => $this->checkoutSessionId,
-                'callback_url' => $this->callbackUri
-            ]),
+                                      'source'             => $this->source,
+                                      'subdomain'          => $this->subdomain,
+                                      'email'              => $this->email,
+                                      'shop_url'           => $this->shopUrl,
+                                      'stripe_checkout_id' => $this->checkoutSessionId,
+                                      'callback_url'       => $this->callbackUri,
+                                  ]),
         ]);
     }
 }
