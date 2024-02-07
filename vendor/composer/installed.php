6ef91121ee0b08141337e910fce875cc16bca83e
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-02-07 13:48:05 +0100

    MKGO-37
    Add Vendor Directory to get Stripe included

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dat.lieu/gambio-connect',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0d1c96a8b3a89e7ad2550f10b75271226d9b6876',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dat.lieu/gambio-connect' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0d1c96a8b3a89e7ad2550f10b75271226d9b6876',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v13.9.0',
+            'version' => '13.9.0.0',
+            'reference' => '3d9250964c9d0bc9de8c706b16a90a82d9c8ed9d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
