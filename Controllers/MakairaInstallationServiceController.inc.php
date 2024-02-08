44d6165e23a041496442ce7be33f5197922b82db
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-02-08 09:36:58 +0100

    MKGO-37
    Fixed key for stripe checkout id

--- Controllers/MakairaInstallationServiceController.inc.php
+++ Controllers/MakairaInstallationServiceController.inc.php
@@ -20,7 +20,7 @@ class MakairaInstallationServiceController extends HttpViewController
 
     public function actionDefault(): \JsonHttpControllerResponse
     {
-        if($this->configurationService->find('modules/MakairaGambioConnect/stripeCheckoutSession')->value() === $this->_getPostData('checkoutSession')) {
+        if($this->configurationService->find('modules/MakairaGambioConnect/stripeCheckoutSession')->value() === $this->_getPostData('stripeCheckoutId')) {
             
             $this->configurationService->save('modules/MakairaGambioConnect/makairaUrl', $this->_getPostData('url'));
             
