1eb294e96437dadbc80b5061619fbd617a22fba5
Ben Sauer <bs@marmalade.group>
2024-02-08 14:20:29 +0100

    MKGO-37  fixed doctrine legacy issue

--- App/GambioConnectService/GambioConnectCategoryService.php
+++ App/GambioConnectService/GambioConnectCategoryService.php
@@ -12,8 +12,8 @@ use GXModules\Makaira\GambioConnect\Service\GambioConnectEntityInterface;
 class GambioConnectCategoryService extends GambioConnectService implements GambioConnectEntityInterface
 {
     private Language $currentLanguage;
-    
-    
+
+
     public function prepareExport(): void
     {
         $languages = $this->languageReadService->getLanguages();
@@ -26,18 +26,18 @@ class GambioConnectCategoryService extends GambioConnectService implements Gambi
             }
         }
     }
-    
+
     public function export(): void
     {
         $languages = $this->languageReadService->getLanguages();
-        
+
         $makairaExports = $this->getEntitiesForExport('category');
-        
-        if(!empty($makairaExports)) {
+
+        if (!empty($makairaExports)) {
             foreach ($languages as $language) {
                 $this->currentLanguage = $language;
                 $categories            = $this->getQuery($language, $makairaExports);
-                
+
                 foreach ($categories as $category) {
                     $this->pushRevision($category);
                     $this->exportIsDone($category['categories_id'], 'category');
@@ -45,8 +45,8 @@ class GambioConnectCategoryService extends GambioConnectService implements Gambi
             }
         }
     }
-    
-    
+
+
     /**
      * @throws Exception
      */
@@ -56,26 +56,26 @@ class GambioConnectCategoryService extends GambioConnectService implements Gambi
 
         $makairaCategory = MakairaDataMapper::mapCategory($category, $hierarchy, $this->currentLanguage);
         $data            = $this->addMakairaDocumentWrapper($makairaCategory, $this->currentLanguage);
-        
+
         $response = $this->client->push_revision($data);
-        
+
         $this->logger->info("Makaira Categories Status for " . $category['categories_id'] . ": "
-                            . $response->getStatusCode());
+            . $response->getStatusCode());
     }
-    
-    
+
+
     public function replace(): void
     {
         $this->client->rebuild(['category']);
     }
-    
-    
+
+
     public function switch(): void
     {
         $this->client->rebuild(['category']);
     }
-    
-    
+
+
     /**
      * @param Language $language
      *
@@ -87,37 +87,43 @@ class GambioConnectCategoryService extends GambioConnectService implements Gambi
         $query = $this->connection->createQueryBuilder()
             ->select('*')
             ->from('categories')
-            ->leftJoin('categories',
-                       'categories_description',
-                       'categories_description',
-                       'categories.categories_id = categories_description.categories_id')
-            ->leftJoin('categories_description',
-                       'languages',
-                       'languages',
-                       'categories_description.language_id = languages.languages_id')
+            ->leftJoin(
+                'categories',
+                'categories_description',
+                'categories_description',
+                'categories.categories_id = categories_description.categories_id'
+            )
+            ->leftJoin(
+                'categories_description',
+                'languages',
+                'languages',
+                'categories_description.language_id = languages.languages_id'
+            )
             ->where('categories_description.language_id = :languageId')
             ->setParameter('languageId', $language->id());
-        
-        if(!empty($makairaChanges)) {
-            $ids = array_map(fn($change) => $change['gambio_id'], $makairaChanges);
+
+        if (!empty($makairaChanges)) {
+            $ids = array_map(fn ($change) => $change['gambio_id'], $makairaChanges);
             $query->where('categories.categories_id IN (:ids)')
                 ->setParameter('ids', implode(',', array_values($ids)));
         }
-        
-        return $this->executeQuery($query);
+
+        return $query->execute()->fetchAllAssociative();
     }
-    
-    
+
+
     private function getSubcategories(int $category_id): array
     {
-        return $this->executeQuery($this->connection->createQueryBuilder()
+        return $this->connection->createQueryBuilder()
             ->select('categories_id')
             ->from('categories')
             ->where('parent_id = :categories_id')
-            ->setParameter('categories_id', $category_id));
+            ->setParameter('categories_id', $category_id)
+            ->execute()
+            ->fetchAllAssociative();
     }
-    
-    
+
+
     private function calculateCategoryDepth(array $category, int $depth = 1, string $hierarchy = ''): array
     {
         if (empty($category['parent_id'])) {
@@ -126,21 +132,23 @@ class GambioConnectCategoryService extends GambioConnectService implements Gambi
                 'hierarchy' => empty($hierarchy) ? $category['categories_id'] : $hierarchy,
             ];
         }
-        
+
         $depth += 1;
-        
-        $parentCategory = $this->executeQuery($this->connection->createQueryBuilder()
+
+        $parentCategory = $this->connection->createQueryBuilder()
             ->select('categories_id, parent_id')
             ->from('categories')
             ->where('categories_id = :parent_id')
-            ->setParameter('parent_id', $category['parent_id']));
-        
+            ->setParameter('parent_id', $category['parent_id'])
+            ->execute()
+            ->fetchAssociative();
+
         if (empty($hierarchy)) {
             $hierarchy = $parentCategory['categories_id'] . '//' . $category['categories_id'];
         } else {
             $hierarchy = $parentCategory['categories_id'] . '//' . $hierarchy;
         }
-        
+
         return $this->calculateCategoryDepth($parentCategory, $depth, $hierarchy);
     }
 }
