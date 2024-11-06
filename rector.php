a8bc1d011c7df22d49e2e20e15d87ed09b16de95
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-11-06 09:03:31 +0100

    Fixed Module Structure

--- rector.php
+++ rector.php
@@ -7,7 +7,7 @@ use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/src',
+        __DIR__.'/src',
     ])
     // uncomment to reach your current PHP version
     ->withPhpVersion(PhpVersion::PHP_80)
